a2ffbde0ae64c03e56773410ed1e35d04c4d5a30
2141064h <114968942+Daisuke0529@users.noreply.github.com>
2022-11-28 21:17:38 +0900

    Change section4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -7,7 +7,7 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
 
   // 横線を引く
@@ -17,8 +17,16 @@ function setup(){
   // ここからが本番
   fill(0);
   const dx = width / scores.length;
-  let px, py; // 線を引くために一つ前の点を覚えておく変数
+  let px=dx/2, py=height-scores[0]*4; // 線を引くために一つ前の点を覚えておく変数
   for(let i = 0; i < scores.length; i++){
     // BLANK[1]
+    if(i==0){
+      ellipse(px,py,10,10);
+    }
+    else{
+      ellipse(px+dx*i,height-scores[i]*4,10,10);
+      line(px+dx*i,height-scores[i]*4,px+dx*(i-1),height-scores[i-1]*4); 
+      
+    }
   }
 }
