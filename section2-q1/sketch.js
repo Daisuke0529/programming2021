78f678e07783b73c51b356a204db7216bd20a06b
2141064h <114968942+Daisuke0529@users.noreply.github.com>
2022-11-28 21:06:20 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  createCanvas(100,100);
+  noFill();
+  for(let i=1;i<11;i++){
+    if(i<6){
+      stroke(0,0,255);
+      ellipse(50,50,i*5);
+      }
+    else{
+      stroke(255,0,0);
+      ellipse(50,50,i*5);
+      }
   }
 }
