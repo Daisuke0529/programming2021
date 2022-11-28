d5b77a629e826beac3d30221ffd1bc34b185a2a3
2141064h <114968942+Daisuke0529@users.noreply.github.com>
2022-11-28 21:07:08 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,22 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-  let size = width / 8;
+  let size=width/8;
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      fill((i+j)%2==0?255:180);
+      rect(i*size,j*size,size,size);
+      let xzahyou=size/2+i*size;
+      let yzahyou=size/2+j*size;
+      if(j<3&&(i+j)%2==1){
+        fill(255,0,0);
+        ellipse(xzahyou,yzahyou,width/10);
+        }
+      else if(j>4&&(i+j)%2==1){
+        fill(0);
+        ellipse(xzahyou,yzahyou,width/10);
+        }
     }
   }
 }
