336b201baa86bc663e14ae5996ce5dffd1d262c0
2141064h <114968942+Daisuke0529@users.noreply.github.com>
2022-11-28 21:18:34 +0900

    Change section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -7,12 +7,25 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(0, 100);// 60以上100未満のランダムな数を代入
   }
+  scores=sort(scores);
+  scores=reverse(scores);
+  console.log(scores);
 
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
-  for(let i = 0; i < scores.length; i++){ total += scores[i]; }
-
-  // BLANK[1]
+  for(let i = 0; i < scores.length; i++){
+    total=total+scores[i];
+  }
+  let start=-PI/2;
+  for(let i=0;i<scores.length;i++){
+    arc(width/2,height/2,width*0.8,height*0.8,start,start+TWO_PI*scores[i]/total,PIE);
+  start=start+TWO_PI*scores[i]/total;
+  console.log(start);
+  }
+  
+  
+  
+  
 }
